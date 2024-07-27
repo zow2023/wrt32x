f335669ed7680c83a8198758ffb9424fa2bfc00c
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-07-27 12:40:22 +0800

    Update before_build.sh

--- scripts/before_build.sh
+++ scripts/before_build.sh
@@ -3,7 +3,7 @@
 # Enter your commands here, e.g.
 # echo "Start build!"
 cd openwrt || exit
-git checkout ebb3faf31f7c34d71e8ffbf3e8d94bf92086188e
+
 #git reset --hard 29a3967e61334d0c6a1a7d391f0e751272d77b1d
 #git revert --no-edit eaa9c94c75748265b9260691d5f59c9d3711f7d4
 
